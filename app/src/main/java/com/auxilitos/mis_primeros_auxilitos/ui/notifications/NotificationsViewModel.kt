76b22e3d72c20400a7a9f8fed72f077fbcc2dd96
Andres Felipe <pizoluligoa@gmail.com>
2022-11-16 08:46:07 -0500

    Se crea proyecto movil

---
+++ app/src/main/java/com/auxilitos/mis_primeros_auxilitos/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.auxilitos.mis_primeros_auxilitos.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
