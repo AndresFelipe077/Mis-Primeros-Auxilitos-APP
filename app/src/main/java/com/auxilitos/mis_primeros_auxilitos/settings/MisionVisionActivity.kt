db81201a3479cce58de9fb9fcacdb223a86f2fbb
AndresFelipe077 <pizoluligoa@gmail.com>
2023-10-05 15:49:14 -0500

    Create view mision vision

---
+++ app/src/main/java/com/auxilitos/mis_primeros_auxilitos/settings/MisionVisionActivity.kt
@@ -0,0 +1,12 @@
+package com.auxilitos.mis_primeros_auxilitos.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.auxilitos.mis_primeros_auxilitos.R
+
+class MisionVisionActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_mision_vision)
+  }
+}
