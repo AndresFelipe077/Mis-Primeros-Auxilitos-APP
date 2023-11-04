e62e1151ec38bb0f87d15b58ca7292995b481320
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-04 18:58:53 -0500

    Add new package of my content and add contentAdapter

---
+++ app/src/main/java/com/auxilitos/mis_primeros_auxilitos/content/my_content/MyContent.kt
@@ -0,0 +1,12 @@
+package com.auxilitos.mis_primeros_auxilitos.content.my_content
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.auxilitos.mis_primeros_auxilitos.R
+
+class MyContent : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_my_content)
+  }
+}
