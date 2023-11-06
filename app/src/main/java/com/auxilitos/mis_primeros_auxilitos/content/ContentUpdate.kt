79182f48b344afadf6e68ddb8c3709f7dafd4172
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-06 10:53:51 -0500

    Update code of getContent

--- app/src/main/java/com/auxilitos/mis_primeros_auxilitos/content/ContentUpdate.kt
+++ app/src/main/java/com/auxilitos/mis_primeros_auxilitos/content/ContentUpdate.kt
@@ -6,17 +6,17 @@ import com.auxilitos.mis_primeros_auxilitos.R
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.auxilitos.mis_primeros_auxilitos.MainActivity
 import com.auxilitos.mis_primeros_auxilitos.classesImport.ToastCustom
 import com.auxilitos.mis_primeros_auxilitos.client.ApiClient
+import com.auxilitos.mis_primeros_auxilitos.content.my_content.MyContentActivity
 import com.auxilitos.mis_primeros_auxilitos.databinding.ActivityContentUpdateBinding
 import com.auxilitos.mis_primeros_auxilitos.model.request.ContentRequest
+import com.auxilitos.mis_primeros_auxilitos.model.response.ContentResponse
 import com.auxilitos.mis_primeros_auxilitos.model.response.User
-import com.auxilitos.mis_primeros_auxilitos.model.response.UserManager
-import com.auxilitos.mis_primeros_auxilitos.registro.Profile
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
@@ -25,14 +25,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.File
-import java.io.FileOutputStream
+import java.lang.Integer.parseInt
 
 class ContentUpdate : AppCompatActivity() {
 
@@ -40,16 +37,20 @@ class ContentUpdate : AppCompatActivity() {
   private val toast = ToastCustom()
   private lateinit var imageUri: Uri
 
-  var userData: User? = null
+  private var contentId = 0
+
+  private var myContent: ContentResponse? = null
+
+  private var userData: User? = null
   private var userId = 0
 
   private val contract = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
     uri?.let {
       imageUri = it
-      binding.imageUrl.setImageURI(it)
+      binding.imageUrlUpdate.setImageURI(it)
 
       val borderDrawable = ContextCompat.getDrawable(this, R.drawable.bordernavigation) // Obtén el fondo redondeado desde los recursos
-      binding.imageUrl.background = borderDrawable // Establece el fondo redondeado en el ImageButton
+      binding.imageUrlUpdate.background = borderDrawable // Establece el fondo redondeado en el ImageButton
 
       val requestOptions = RequestOptions()
         .placeholder(R.drawable.image_preview) // Imagen de placeholder mientras se carga la imagen
@@ -60,7 +61,7 @@ class ContentUpdate : AppCompatActivity() {
         .load(imageUri)
         .apply(requestOptions)
         .centerCrop() // Escala la imagen para llenar el área del ImageButton mientras mantiene las proporciones y corta el exceso
-        .into(binding.imageUrl)
+        .into(binding.imageUrlUpdate)
 
     }
   }
@@ -71,71 +72,28 @@ class ContentUpdate : AppCompatActivity() {
     binding = ActivityContentUpdateBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    val intent = intent
-    if (intent != null && intent.hasExtra("CONTENIDO_ID")) {
-      val contenidoId = intent.getStringExtra("CONTENIDO_ID")
-      Log.d("Contenido ID", "ID del contenido: $contenidoId")
-    } else {
-      toast.toastError(this, "Error", "Ups!, ha ocurrido un error inesperado, intentalo de nuevo o más tarde")
-    }
+    getIdMyContent()
+
+    getMyContentById(contentId)
 
     binding.btnChooseImage.setOnClickListener {
       contract.launch("image/*")
     }
 
     binding.btnReturn.setOnClickListener {
-      startActivity(Intent(this, Profile::class.java))
+      startActivity(Intent(this, MyContentActivity::class.java))
     }
 
-    createContent()
+    //updateContent()
   }
 
-
-  @SuppressLint("Recycle")
-  private fun createContent() {
-
-    userId = UserManager.getUserId()
-
-    getUserProfile(userId.toString())
-
-    binding.btnUploadContent.setOnClickListener {
-
-      val filesDir = applicationContext.filesDir
-      val file = File(filesDir, "image.png")
-
-      val inputStream = imageUri.let { contentResolver.openInputStream(it) }
-      val outputStream = FileOutputStream(file)
-      inputStream!!.copyTo(outputStream)
-
-      val requestBody = file.asRequestBody("image/*".toMediaTypeOrNull())
-      val part = MultipartBody.Part.createFormData("url", file.name, requestBody)
-
-      val title = binding.title.text.toString()
-      val description = binding.description.text.toString()
-
-      if (title.isNotEmpty() && description.isNotEmpty()) {
-
-        // Object of content
-        val contentRequest = userData?.let { user ->
-          ContentRequest(
-            title,
-            part,
-            autor = user.name,
-            description,
-            user_id = user.id
-          )
-        }
-
-        // Llamar a la función para enviar los datos al servidor
-        if (contentRequest != null) {
-          updateContent(contentRequest)
-        }
-
-      } else {
-
-        toast.toastWarning(this, "Campos incompletos", "Completa los campos y selecciona una imagen")
-
-      }
+  private fun getIdMyContent()
+  {
+    val intent = intent
+    if (intent != null && intent.hasExtra("CONTENIDO_ID")) {
+      contentId = intent.getStringExtra("CONTENIDO_ID")?.let { parseInt(it) }!!
+    } else {
+      toast.toastError(this, "Error", "Ups!, ha ocurrido un error inesperado, intentalo de nuevo o más tarde")
     }
   }
 
@@ -197,6 +155,34 @@ class ContentUpdate : AppCompatActivity() {
       }
 
       override fun onFailure(call: Call<User>, t: Throwable) {
+        toast.toastError(this@ContentUpdate, "Conexión", "Error de conexión")
+      }
+    })
+  }
+
+  private fun getMyContentById(contentId: Int)
+  {
+    val apiService = ApiClient.getApiService()
+
+    val myContentCall: Call<ContentResponse> = apiService.getOneContent(contentId.toString())
+    myContentCall.enqueue(object : Callback<ContentResponse> {
+      @SuppressLint("SetTextI18n")
+      override fun onResponse(call: Call<ContentResponse>, response: Response<ContentResponse>) {
+        if (response.isSuccessful) {
+          val myContent = response.body()
+          myContent?.let {
+            findViewById<TextView>(R.id.title_update).text          = it.title
+            findViewById<TextView>(R.id.description_update).text    = it.description
+            Glide.with(this@ContentUpdate)
+              .load(ApiClient.baseUrl + it.url)
+              .placeholder(R.drawable.logo) // Imagen de carga mientras se carga la imagen
+              .error(R.drawable.logo) // Imagen de error si no se puede cargar la imagen
+              .into(binding.imageUrlUpdate)
+          }
+        }
+      }
+
+      override fun onFailure(call: Call<ContentResponse>, t: Throwable) {
         toast.toastError(this@ContentUpdate, "Conexión", "Error de conexión")
       }
     })
